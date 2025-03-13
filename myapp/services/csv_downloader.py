750e8bcd7c0fae404d8144729a54d7108254858b
ElMonkito <rh@RT-QLS-NBK-022.ad.reist-tele.com>
2025-03-13 08:14:28 +0100

    change csv file

--- myapp/services/csv_downloader.py
+++ myapp/services/csv_downloader.py
@@ -1,54 +1,36 @@
 import os
-import logging
 from playwright.sync_api import sync_playwright
-from apscheduler.schedulers.background import BackgroundScheduler
-import time
 
 UPLOADS_FOLDER = os.path.abspath("../../uploads")
 FIXED_FILENAME = "matchs.csv"
 
-# Configuration des logs
-logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
-
 
 def download_csv():
-    """Télécharge le fichier CSV et le sauvegarde dans le bon dossier."""
-    logging.info("Début du téléchargement du fichier CSV...")
-
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless=True)
+        browser = p.chromium.launch(headless=False)
         page = browser.new_page()
 
         page.goto("https://www.sihf.ch/fr/game-center/other-leagues#/today/time/asc/page/0/")
+
         page.wait_for_load_state("networkidle")
 
         with page.expect_download() as download_info:
             page.click(".fa.fa-table.u-pull-right")
 
         download = download_info.value
+
         fixed_path = os.path.join(UPLOADS_FOLDER, FIXED_FILENAME)
 
         if os.path.exists(fixed_path):
             os.remove(fixed_path)
-            logging.info("Ancien fichier supprimé.")
+            print("Delete old file.")
 
         download.save_as(fixed_path)
-        logging.info(f"CSV téléchargé avec succès : {fixed_path}")
+        print(f"CSV file download successfuly : {fixed_path}")
 
         browser.close()
 
 
 os.makedirs(UPLOADS_FOLDER, exist_ok=True)
 
-scheduler = BackgroundScheduler()
-scheduler.add_job(download_csv, "cron", hour=1, minute=0)
-scheduler.start()
-
-logging.info("Planificateur APScheduler démarré. Le script tournera en tâche de fond.")
-
-try:
-    while True:
-        time.sleep(60)
-except (KeyboardInterrupt, SystemExit):
-    logging.info("Arrêt du script...")
-    scheduler.shutdown()
+download_csv()
